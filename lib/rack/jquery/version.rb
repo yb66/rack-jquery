89363b18a48f752838c628be9d978c4ec4b795c4
Iain Barnett <iainspeed@gmail.com>
2013-03-03 13:52:32 +0000

    Added all the headers needed for caching.

--- lib/rack/jquery/version.rb
+++ lib/rack/jquery/version.rb
@@ -1,5 +1,8 @@
 module Rack
   class JQuery
     VERSION = "1.9.1"
+
+    # TODO remember to change Last-Modified with each release!
+    JQUERY_VERSION_DATE = "Mon, 04 Feb 2013 00:00:00 GMT"
   end
 end
