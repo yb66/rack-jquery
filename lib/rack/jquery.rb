5b4991369e420ae1942c8fb35fa06644748dad69
Iain Barnett <iainspeed@gmail.com>
2013-03-05 13:11:45 +0000

    Was missing end of script tag for fallback. Fixed.

--- lib/rack/jquery.rb
+++ lib/rack/jquery.rb
@@ -20,7 +20,7 @@ module Rack
     FALLBACK = <<STR
 <script type="text/javascript">
   if (typeof jQuery == 'undefined') {
-      document.write(unescape("%3Cscript src='/js/#{JQUERY_FILE_NAME}' type='text/javascript'%3E))
+    document.write(unescape("%3Cscript src='/js/#{JQUERY_FILE_NAME}' type='text/javascript'%3E%3C/script%3E"))
   };
 </script>
 STR
