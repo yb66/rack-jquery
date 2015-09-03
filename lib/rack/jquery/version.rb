4348ef3074bb9947400aca9394d2838c4587fcb0
Iain Barnett <iainspeed@gmail.com>
2015-09-03 14:34:18 +0100

    Updated JQuery versions etc in lib.

--- lib/rack/jquery/version.rb
+++ lib/rack/jquery/version.rb
@@ -5,10 +5,10 @@ module Rack
     VERSION = "2.2.1"
 
     # the version of jQuery it supports.
-    JQUERY_VERSION = "2.1.0"
+    JQUERY_VERSION = "2.1.4"
 
     # This is the release date of the jQuery file, it makes an easy "Last-Modified" date for setting the headers around caching.
     # @todo remember to change Last-Modified with each release!
-    JQUERY_VERSION_DATE = "Thu, 23 Jan 2014 21:11:05 +0000" 
+    JQUERY_VERSION_DATE = "Tue, 28 Apr 2015 12:24:00 +0100"
   end
 end
